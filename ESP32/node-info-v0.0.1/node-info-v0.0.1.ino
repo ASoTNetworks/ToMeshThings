4620f7718316d27db0c702cae1882ec8088acbae
ASoT Networks <dev@aseriesoftubez.com>
2019-04-19 03:55:13 -0400

    cjdns working and WiFi stored in EEPROM

--- ESP32/node-info-v0.0.1/node-info-v0.0.1.ino
+++ ESP32/node-info-v0.0.1/node-info-v0.0.1.ino
@@ -1,3 +1,4 @@
+#include "esp_system.h"
 #include "WiFi.h"
 #include <U8x8lib.h>
 #include <iostream>
@@ -6,13 +7,16 @@
 #include <Time.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
+#include <EEPROM.h>
 
 // OLED config
 U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
 
-// WiFi config
-#define STA_SSID "tomesh-node"
-#define STA_PASS "password"
+// display scroll delay in ms
+#define displayScroll 2000
+
+// display contrast 0 to 255
+#define displayContrast 0
 
 // NTP config
 // timezone in seconds
@@ -28,27 +32,158 @@ String httpYPath = "/cgi-bin/peers-yggdrasil";
 
 // put values into yurl
 String yurl=("http://" + httpYHost + httpYPath);
+
+// cjdns HTTP vars
+String httpCHost = "10.0.0.1";
+String httpCPath = "/cgi-bin/peers-cjdns-arduino";
+
+// wifi vars (do not set your WiFi password here it will be set in EEPROM)
+String STA_SSID = "";
+String STA_PASS = "";
+
+// put values into curl
+String curl=("http://" + httpCHost + httpCPath);
+
 static volatile bool wifi_connected = false;
 
 // defines
 String mac = "";
+int prg = 0;
+int forceprg = 0;
+String WaitForInput(String Question) {
+  Serial.println(Question);
+  while(!Serial.available()) {
+    // wait for input
+  }
+  return Serial.readStringUntil(10);
+}
 
 // NTP function
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
 
 void setup(){
+    // init the serial console
     Serial.begin(115200);
+
+    // init the EEPROM
+    EEPROM.begin(512);
+
+    // init the OLED
+    u8x8.begin();
+    u8x8.setFont(u8x8_font_chroma48medium8_r);
+    u8x8.setContrast(displayContrast);
+
+    // init GPIOs
     pinMode(25, OUTPUT);
+    pinMode(0, INPUT);
+    
+    // prompt for program mode
+    u8x8.clear();
+    u8x8.setCursor(0, 0);
+    u8x8.printf("Hold PRG button");
+    u8x8.setCursor(0, 1);
+    u8x8.printf("to clear EEPROM");
+    u8x8.setCursor(0, 2);
+    u8x8.printf("and re-program.");
+    delay(2000);
+
+    // get wifi config from eeprom
+    // get SSID from eeprom
+    String essid = "";
+    for (int i = 0; i < 32; ++i){
+      essid += char(EEPROM.read(i));
+    }
+    // get password from eeprom
+    String epasswd = "";
+    for (int i = 32; i < 96; ++i){
+      epasswd += char(EEPROM.read(i));
+    }
+    // defines for wifi client
+    STA_SSID = essid;
+    STA_PASS = epasswd;
+
+    // check if values are blank if it is go into programming mode
+    if ((essid == "") || (epasswd == "")) {
+      forceprg = 1;
+    }
+    // go into programming mode when PRG button is pressed.
+    prg = digitalRead(0);
+    if ((prg == LOW) || (forceprg == 1)){
+      Serial.println("Entered Programming Mode");
+      // turn LED on
+      digitalWrite(25, HIGH);
+
+      // print programming mode on OLED
+      u8x8.clear();
+      u8x8.setCursor(0, 0);
+      u8x8.printf("Program Mode");
+      u8x8.setCursor(0, 1);
+      u8x8.printf("Open serial");
+      u8x8.setCursor(0, 2);
+      u8x8.printf("console and");
+      u8x8.setCursor(0, 3);
+      u8x8.printf("press ENTER or");
+      u8x8.setCursor(0, 4);
+      u8x8.printf("Send.");
+      u8x8.setCursor(0, 6);
+      u8x8.printf("Baud rate");
+      u8x8.setCursor(0, 7);
+      u8x8.printf("115200");
+
+      // clear EEPROM
+      Serial.println("Clearing EEPROM...");
+      for (int i = 0; i < 96; ++i) { EEPROM.write(i, 0); }
+      EEPROM.commit();
+      Serial.println("EEPROM Cleared!");
+
+      // user serial prompt
+      // press enter to start
+      WaitForInput("Press enter or Send button to start.");
+
+      // ask SSID
+      String qssid;
+      qssid = WaitForInput("Please enter your SSID");
+      Serial.print("SSID set to: ");
+      Serial.println(qssid);
+
+      // ask password
+      String qpasswd;
+      qpasswd = WaitForInput("Please enter your password");
+      Serial.println("password stored");
+
+      // write data to EEPROM
+      // SSID
+      Serial.println("writing SSID to EEPROM:");
+      for (int i = 0; i < qssid.length(); ++i){
+        EEPROM.write(i, qssid[i]);
+      }
+      // passwd
+      Serial.println("writing passwd to EEPROM:");
+      for (int i = 0; i < qpasswd.length(); ++i){
+        EEPROM.write(32+i, qpasswd[i]);
+      }
+      EEPROM.commit();
+      Serial.println("EEPROM Programmed. Rebooting.");
+      
+      // reboot
+      delay(500);
+      Serial.println("Rebooting");
+      digitalWrite(25, LOW);
+      esp_restart();
+    }
+
+    // init wifi
     WiFi.disconnect(true);
     WiFi.onEvent(WiFiEvent);
     WiFi.mode(WIFI_MODE_STA);
-    WiFi.begin(STA_SSID, STA_PASS);
+    WiFi.begin(essid.c_str(), epasswd.c_str());
     mac=WiFi.macAddress();
+
+    // start NTP client
     timeClient.begin();
-    u8x8.begin();
-    u8x8.setFont(u8x8_font_chroma48medium8_r);
-    u8x8.setContrast(0);
+
+    // print starting on OLED
     u8x8.clear();
     u8x8.setCursor(0, 0);
     u8x8.print("Starting...");
@@ -95,7 +230,20 @@ void wifiOnDisconnect(){
     u8x8.setCursor(0, 1);
     u8x8.print("Connection Fail");
     delay(1000);
-    WiFi.begin(STA_SSID, STA_PASS);
+    
+    // get wifi config from eeprom
+    // get SSID from eeprom
+    String essid = "";
+    for (int i = 0; i < 32; ++i){
+      essid += char(EEPROM.read(i));
+    }
+    // get password from eeprom
+    String epasswd = "";
+    for (int i = 32; i < 96; ++i){
+      epasswd += char(EEPROM.read(i));
+    }
+    // defines for wifi client
+    WiFi.begin(essid.c_str(), epasswd.c_str());
 }
 
 void WiFiEvent(WiFiEvent_t event){
@@ -118,7 +266,7 @@ void WiFiEvent(WiFiEvent_t event){
 }
 
 void wifiConnectedLoop(){
-    // turn LED on to indicate loop is running
+    // turn LED on to indicate processing date
     digitalWrite(25, HIGH);
     unsigned long epoch = timeClient.getEpochTime();
     Serial.print("");
@@ -132,28 +280,37 @@ void wifiConnectedLoop(){
     unsigned long cminute = minute(utcCalc );
     unsigned long csecond = second(utcCalc );
     Serial.printf("Current Time: %04lu-%02lu-%02lu %02lu:%02lu:%02lu\n", cyear,cmonth,cday,chour,cminute,csecond);
-    
+    u8x8.clear();
+    u8x8.setCursor(0, 0);
+    u8x8.printf("Current Time is");
+    u8x8.setCursor(0, 1);
+    u8x8.printf("%04lu-%02lu-%02lu\n%02lu:%02lu", cyear,cmonth,cday,chour,cminute);
+    // turn LED off to indicate processing date done
+    digitalWrite(25, LOW);
+    delay(displayScroll);
+
+    // turn LED on to indicate processing Yggdrasil data
+    digitalWrite(25, HIGH);
     // get Yggdrasil data from node over HTTP
     // clear screen and print Yggdrasil peers
     u8x8.clear();
     u8x8.setCursor(0, 0);
     u8x8.printf("Yggdrasil Peers");
-    delay(500);
     
     // object of class HTTPClient
-    Serial.println("Starting HTTPClient http");
-    HTTPClient http;
-    Serial.println("http.begin: " + yurl);
-    http.begin(yurl);
-    Serial.println("http.GET");
-    int httpCode = http.GET();
+    Serial.println("Starting HTTPClient yhttp");
+    HTTPClient yhttp;
+    Serial.println("yhttp.begin: " + yurl);
+    yhttp.begin(yurl);
+    Serial.println("yhttp.GET");
+    int yhttpCode = yhttp.GET();
     int ycount=0;
-    int line=0;
+    int yline=0;
     
-    //Check the returning code                                                                  
-    if (httpCode > 0) {
+    //Check the returning code
+    if (yhttpCode > 0) {
       // Get the request response payload
-      String ydata = http.getString();
+      String ydata = yhttp.getString();
       
       // output RAW data
       Serial.println("RAW DATA");
@@ -165,13 +322,17 @@ void wifiConnectedLoop(){
       if (error) {
         Serial.print(F("deserializeJson() failed: "));
         Serial.println(error.c_str());
+        u8x8.setCursor(0, 1);
+        u8x8.printf("deserializeJson\nfailed:\n%s", error.c_str());
+        digitalWrite(25, LOW);
+        delay(displayScroll);
         return;
       }
 
       JsonObject obj=doc["peers"].as<JsonObject>();
       for (JsonObject::iterator it=obj.begin(); it!=obj.end(); ++it) {
         ycount++;
-        line++;
+        yline++;
         Serial.print("Peer: ");
         Serial.print(it->key().c_str());
         Serial.print(" Endpoint ");
@@ -184,14 +345,14 @@ void wifiConnectedLoop(){
         // output stuff to the screen 
         char *yspeer = strrchr(it->key().c_str(), ':');
         if (yspeer != NULL) {
-          if (line <= 7){
-            Serial.printf("line %d %d %s\n",line,ycount,yspeer+1);
-            u8x8.clearLine(line);
-            u8x8.setCursor(0, line);
+          if (yline <= 7){
+            Serial.printf("line %d %d %s\n",yline,ycount,yspeer+1);
+            u8x8.clearLine(yline);
+            u8x8.setCursor(0, yline);
             u8x8.printf("%s\n",yspeer+1);
           }
           else {
-            delay(2000);
+            delay(displayScroll);
             u8x8.clearLine(1);
             u8x8.clearLine(2);
             u8x8.clearLine(3);
@@ -199,62 +360,95 @@ void wifiConnectedLoop(){
             u8x8.clearLine(5);
             u8x8.clearLine(6);
             u8x8.clearLine(7);
-            line=1;
-            Serial.printf("line %d %d %s\n",line,ycount,yspeer+1);
-            u8x8.setCursor(0, line);
+            yline=1;
+            Serial.printf("line %d %d %s\n",yline,ycount,yspeer+1);
+            u8x8.setCursor(0, yline);
             u8x8.printf("%s\n",yspeer+1);
           }
         }
       }
     }
     //Close connection
-    http.end();
-    delay(2000);
+    yhttp.end();
+    // turn LED off to indicate done processing Yggdrasil data
+    digitalWrite(25, LOW);
+    delay(displayScroll);
 
+    // cjdns
+    // turn LED on to indicate processing cjdns data
+    digitalWrite(25, HIGH);
+    // get Yggdrasil data from node over HTTP
+    // clear screen and print cjdns peers
+    u8x8.clear();
+    u8x8.setCursor(0, 0);
+    u8x8.printf("cjdns Peers");
     
-    // output stuff to screen
-    //u8x8.clear();
-    //u8x8.clearLine(0);
-    //u8x8.setCursor(0, 0);
-    //u8x8.printf("%04lu-%02lu-%02lu\n%02lu:%02lu:%02lu\n", cyear,cmonth,cday,chour,cminute,csecond);
-    // output ygg peers to screen
-    /*
-    for (int i = 0; i <= ycount; i++) {
-      const char* arr_ypeer[i];
-      Serial.println(arr_ypeer[i]);
-      char *yspeer = strrchr(arr_ypeer[i], ':');
-      if (yspeer != NULL) {
-        Serial.printf("%d %s\n",i,yspeer+1);
+    // object of class HTTPClient
+    Serial.println("Starting HTTPClient http");
+    HTTPClient chttp;
+    Serial.println("chttp.begin: " + curl);
+    chttp.begin(curl);
+    Serial.println("chttp.GET");
+    int chttpCode = chttp.GET();
+    int ccount=0;
+    int cline=0;
+    
+    //Check the returning code
+    if (chttpCode > 0) {
+      // Get the request response payload
+      String cdata = chttp.getString();
+      
+      // output RAW data
+      Serial.println("RAW DATA");
+      Serial.println(cdata);
+
+      // Parsing
+      StaticJsonDocument<5000> doc;
+      DeserializationError error = deserializeJson(doc, cdata);
+      if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.c_str());
+        u8x8.setCursor(0, 1);
+        u8x8.printf("deserializeJson\nfailed:\n%s", error.c_str());
+        digitalWrite(25, LOW);
+        delay(displayScroll);
+        return;
+      }
+
+      JsonObject obj=doc["peers"].as<JsonObject>();
+      for (JsonObject::iterator it=obj.begin(); it!=obj.end(); ++it) {
+        ccount++;
+        cline++;
+        Serial.print("Peer: ");
+        Serial.print(it->key().c_str());
+
+        // output stuff to the screen 
+        char *cspeer = strrchr(it->key().c_str(), ':');
+        if (cspeer != NULL) {
+          if (yline <= 7){
+            u8x8.clearLine(cline);
+            u8x8.setCursor(0, cline);
+            u8x8.printf("%s\n",cspeer+1);
+          }
+          else {
+            delay(displayScroll);
+            u8x8.clearLine(1);
+            u8x8.clearLine(2);
+            u8x8.clearLine(3);
+            u8x8.clearLine(4);
+            u8x8.clearLine(5);
+            u8x8.clearLine(6);
+            u8x8.clearLine(7);
+            cline=1;
+            u8x8.setCursor(0, cline);
+            u8x8.printf("%s\n",cspeer+1);
+          }
+        }
       }
     }
-    */
-    /*
-    u8x8.clearLine(1);
-    u8x8.setCursor(0, 1);
-    u8x8.print("");
-    u8x8.clearLine(2);
-    u8x8.setCursor(0, 2);
-    u8x8.print("");
-    u8x8.clearLine(3);
-    u8x8.setCursor(0, 3);
-    u8x8.print("");
-    u8x8.clearLine(4);
-    u8x8.setCursor(0, 4);
-    u8x8.print("");
-    u8x8.clearLine(5);
-    u8x8.setCursor(0, 5);
-    u8x8.print("");
-    u8x8.clearLine(6);
-    u8x8.setCursor(0, 6);
-    u8x8.print("");
-    u8x8.clearLine(7);
-    u8x8.setCursor(0, 7);
-    u8x8.print("");
-    */
-
-    // turn LED off to indicate end of the loop
+    //Close connection
+    chttp.end();
+    // turn LED off to indicate done processing cjdns data
     digitalWrite(25, LOW);
-
-    // set how long to wait before updating again in ms
-    delay(5000);
+    delay(displayScroll);
 }
